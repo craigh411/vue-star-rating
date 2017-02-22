993cab6a464a088bd52a51fe45dd4c7c2f745f34
Manuel Schulz <manuel.schulz@peakwork.com>
2017-02-22 10:09:09 +0100

    Cleaned formatting/indents

--- src/star-rating.vue
+++ src/star-rating.vue
@@ -10,117 +10,117 @@
 </template>
 
 <script type="text/javascript">
-    import star from './star.vue';
-    export default {
-        components: {
-            star
+import star from './star.vue';
+export default {
+    components: {
+        star
+    },
+    props: {
+        increment: {
+            default: 1
         },
-        props: {
-            increment: {
-                default: 1
-            },
-            rating: {
-                default: 0
-            },
-            activeColor: {
-                default: "#ffd055"
-            },
-            inactiveColor: {
-                default: "#d8d8d8"
-            },
-            maxRating: {
-                default: 5
-            },
-            starSize: {
-                default: 50
-            },
-            showRating: {
-                default: true
-            },
-            readOnly: {
-                default: false
-            },
-            textClass: {
-                default: ''
+        rating: {
+            default: 0
+        },
+        activeColor: {
+            default: "#ffd055"
+        },
+        inactiveColor: {
+            default: "#d8d8d8"
+        },
+        maxRating: {
+            default: 5
+        },
+        starSize: {
+            default: 50
+        },
+        showRating: {
+            default: true
+        },
+        readOnly: {
+            default: false
+        },
+        textClass: {
+            default: ''
+        }
+    },
+    created() {
+        this.step = this.increment * 100;
+        this.currentRating = this.rating;
+        this.selectedRating = this.rating;
+        this.createStars();
+    },
+    methods: {
+        setRating($event, persist) {
+            if (!this.readOnly) {
+                let position = $event.position / 100;
+                this.currentRating = (($event.id + position) - 1).toFixed(2);
+                this.currentRating = (this.currentRating > this.maxRating) ? this.maxRating : this.currentRating;
+                this.createStars();
+                if (persist) {
+                    this.selectedRating = this.currentRating;
+                    this.$emit('rating-selected', this.selectedRating);
+                } else {
+                    this.$emit('current-rating', this.currentRating);
+                }
             }
         },
-        created() {
-            this.step = this.increment * 100;
-            this.currentRating = this.rating;
-            this.selectedRating = this.rating;
-            this.createStars();
-        },
-        methods: {
-            setRating($event, persist) {
-                if (!this.readOnly) {
-                    let position = $event.position / 100;
-                    this.currentRating = (($event.id + position) - 1).toFixed(2);
-                    this.currentRating = (this.currentRating > this.maxRating) ? this.maxRating : this.currentRating;
-                    this.createStars();
-                    if (persist) {
-                        this.selectedRating = this.currentRating;
-                        this.$emit('rating-selected', this.selectedRating);
-                    } else {
-                        this.$emit('current-rating', this.currentRating);
-                    }
-                }
-            },
-            resetRating() {
-                if (!this.readOnly) {
-                    this.currentRating = this.selectedRating;
-                    this.createStars();
-                }
-            },
-            createStars() {
-                this.fillLevel = [];
-                this.round();
-                for (var i = 0; i < this.maxRating; i++) {
-                    let level = 0;
-                    if (i < this.currentRating) {
-                        level = (this.currentRating - i > 1) ? 100 : (this.currentRating - i) * 100;
-                    }
-                    this.fillLevel[i] = Math.round(level);
-                }
-            },
-            round() {
-                var inv = 1.0 / this.increment;
-                this.currentRating = Math.ceil(this.currentRating * inv) / inv;
-            }
-        },
-        watch: {
-            rating(val) {
-                this.currentRating = val;
-                this.selectedRating = val;
+        resetRating() {
+            if (!this.readOnly) {
+                this.currentRating = this.selectedRating;
                 this.createStars();
             }
         },
-        data() {
-            return {
-                step: 0,
-                fillLevel: [],
-                currentRating: 0,
-                selectedRating: 0
+        createStars() {
+            this.fillLevel = [];
+            this.round();
+            for (var i = 0; i < this.maxRating; i++) {
+                let level = 0;
+                if (i < this.currentRating) {
+                    level = (this.currentRating - i > 1) ? 100 : (this.currentRating - i) * 100;
+                }
+                this.fillLevel[i] = Math.round(level);
             }
+        },
+        round() {
+            var inv = 1.0 / this.increment;
+            this.currentRating = Math.ceil(this.currentRating * inv) / inv;
+        }
+    },
+    watch: {
+        rating(val) {
+            this.currentRating = val;
+            this.selectedRating = val;
+            this.createStars();
+        }
+    },
+    data() {
+        return {
+            step: 0,
+            fillLevel: [],
+            currentRating: 0,
+            selectedRating: 0
         }
     }
+}
 </script>
 
 <style scoped>
-    .star {
-        display: inline-block;
-    }
+.star {
+    display: inline-block;
+}
 
-    .pointer {
-        cursor: pointer;
-    }
+.pointer {
+    cursor: pointer;
+}
 
-    .stars-rating {
-        display: inline-flex;
-        align-items: center;
-    }
+.stars-rating {
+    display: inline-flex;
+    align-items: center;
+}
 
-    .rating-text {
-        margin-left: 7px;
-        margin-top: 7px;
-    }
+.rating-text {
+    margin-left: 7px;
+    margin-top: 7px;
+}
 </style>
