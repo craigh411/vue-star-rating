67a7f2a1e1015e405941d25c8bc48a3185bc2580
Craig Humphreys <craig_h_411@yahoo.co.uk>
2020-03-13 14:13:56 +0000

    fix merge conflicts

--- examples/commonjs/App.vue
+++ examples/commonjs/App.vue
@@ -1,10 +1,6 @@
 <template>
     <div>
-<<<<<<< HEAD
         <star-rating v-model="rating" :increment="0.5" :round-start-rating="false" :clearable="true" />
-=======
-        <star-rating v-model="rating" :round-start-rating="false" />
->>>>>>> master
         <a href="#" @click.prevent="rating = 3.5">Reset</a>
     </div>
 </template>
@@ -15,23 +11,10 @@ export default {
     components: {
         StarRating
     },
-<<<<<<< HEAD
     data(){
     	return{
     		rating: 3.5
     	}
-=======
-    created() {
-       setTimeout(() => {
-            this.rating = 3.5
-        }, 1000)
-
-    },
-    data() {
-        return {
-            rating: 0
-        }
->>>>>>> master
     }
 }
 </script>
