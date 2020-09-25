96118d07fbe6e8c36044cfb72655d01cd3545126
craig.humphreys.ai@gmail.com <craig.humphreys.ai@gmail.com>
2020-09-25 15:31:33 +0100

    lint example

--- examples/commonjs/App.vue
+++ examples/commonjs/App.vue
@@ -1,10 +1,17 @@
 <template>
-    <div>
-        <star-rating v-model:rating="rating" :increment="0.5" :round-start-rating="false" :clearable="true" />
-        <a href="#" @click.prevent="rating = 3.5">Reset</a>
-dd
-        {{rating}}
-    </div>
+  <div>
+    <star-rating
+      v-model:rating="rating"
+      :increment="0.5"
+      :round-start-rating="false"
+      :clearable="true"
+    />
+    <a
+      href="#"
+      @click.prevent="rating = 3.5"
+    >Reset</a>
+    {{ rating }}
+  </div>
 </template>
 <script type="text/javascript">
 import StarRating from '../../src/star-rating.vue';
